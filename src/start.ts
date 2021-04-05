bf3e4353e743828398d6e1e61324d2a6c5af3ee7
José Juan Gázquez <josejuan.gazquez@intelygenz.com>
2021-04-05 16:30:53 +0200

    New: Add cors

--- src/start.ts
+++ src/start.ts
@@ -1,3 +1,4 @@
+import cors from 'cors'
 import express from "express";
 import getRouter from "./routers";
 import {Server} from "http";
@@ -6,6 +7,7 @@ export function startServer({port = process.env.PORT} = {}): Promise<Server> {
     const app = express()
 
     app.use(express.json())
+    app.use(cors())
     app.use('/api', getRouter())
 
     return new Promise(resolve => {
